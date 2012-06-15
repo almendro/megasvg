e52af2684c21a207d2ad71415bad45f7d53b5ac3
almendro <martin@ensamble.com.ar>
2012-06-14 23:35:46 -0300

    El menú circular ahora funciona con elementos DIV, leer documentación en https://github.com/almendro/circle-menu quedan muchos ajustes por realizar, pero el funcionamiento básico está. El archivod ext-mega_gui.js tiene varios comentarios al respecto.
    Falta pasar los cambios que se probaron en mega_gui a la repo en github del circle-menu que hice, que tienen que ver con poder definir el elemento que muestra/oculta el menú circular. En el original era el firt-child pero ahoa se puede definir cualquiera con un class.
    
    Falta algún método para 'desactivar' el objeto circleMenu si se quiere volver 'normal' al menú cuando se desactiva mega-gui. Esto por ahora se logra con un if sobre el class .circular_menu

--- proyecto/editor/extensions/ext-mega_gui.js
+++ proyecto/editor/extensions/ext-mega_gui.js
@@ -14,7 +14,22 @@
 
 
 svgEditor.addExtension("btn_mega_gui", function(s) {
-	$('<link rel="stylesheet" type="text/css" href="extensions/mega_gui/estilos.css" />').appendTo('head');
+
+	var url_extension = "extensions/mega_gui/";
+	
+	jQuery('<link rel="stylesheet" type="text/css" href="'+url_extension+'estilos.css" />').appendTo('head');
+	jQuery('<link rel="stylesheet" type="text/css" href="'+url_extension+'circleMenu.css" />').appendTo('head');
+	jQuery('<script type="text/javascript" src="'+url_extension+'jQuery.circleMenu.js"></script>').appendTo('head');
+	
+	jQuery('html').keyup(function(e) {
+		//alert('');
+	});
+	
+	// Es necesario crear un botón que sirva para activar el menú circular. Luego hay que cambiarlo por otra cosa. El class 'item_primero' es obligadorio, el plugin circleMenu lo busca.
+	
+	jQuery('#tools_top').prepend('<div class="item_primero"><a href="#">[M]</a></div>');
+
+	
 	return {
 		name: "btn_mega_gui",
 		svgicons: "extensions/ext-mega_gui-icon.xml",
@@ -25,13 +40,64 @@ svgEditor.addExtension("btn_mega_gui", function(s) {
 			title: "Activar/Desactivar MEGA GUI []",
 			events: {
 				'click': function() { 
-						if (!$('#btn_mega_gui').hasClass('push_button_pressed')) {
+						if (!jQuery('#btn_mega_gui').hasClass('push_button_pressed')) {
 							// activa la mega-gui
-							$('#btn_mega_gui').addClass('push_button_pressed');
-							$('body').attr('id','mega_gui');
+							jQuery('#btn_mega_gui').addClass('push_button_pressed');
+							jQuery('body').attr('id','mega_gui');
+							
+							
+							// Al principio todas los grupos de botones tiene que tener un posicionamiento absoluto y un z-index bajo para que la div.item_primero esté por encima de todo. Luego hay que agregar a los botones (push_button y tool_button) el class 'item' para que el plugin circleMenu los identifique. En este punto está tomando a todos, tanto los visible como los no visible. Habrá que buscar alguna solución para aprovechar bien el espacio.
+							
+							// El elemento que funciona como menú circular tiene que tener el class 'circle_menu' ya que se utiliza para permutar el estado si mega_gui está activado o no. Y de esta manera el plugin de circleMenu sabe si debe actualizar el aspecto o no. Atención, falta deshabilitar el botón de 'item_primero' cuando se desactiva mega_gui ya que puede ocacionar problemas. Seguramente al ocultaro esto se soluciona.
+							
+							// Al desactivar mega_gui también hay que limpiar estilos que se agregar a los elementos con el atributo 'style' y de esta manera restaurar el aspecto original del css. Se utiliza un 'setTimeout' porque hay que esperar un momento. Esto se puede solucionar si se revisan los timeout del plugin circleMenu.
+							
+							'
+							jQuery('#tools_top > div').css({
+								'position':'absolute',
+								'z-index': '1'
+							});
+							
+							jQuery('#tools_top .push_button, #tools_top .tool_button').addClass('item');
+							
+							jQuery('#tools_top').addClass('circle_menu').circleMenu({
+								circle_radius: 190,
+								item_diameter: 32,
+		            direction:'full', 
+		            trigger:'click',
+		            div: true,
+		            item_primero: 'item_primero',
+		            open: function(){
+		            	//console.log('menu opened');
+		            },
+		            close: function(){
+		            	//console.log('menu closed');
+		            },
+		            init: function(){
+		            	//console.log('menu initialized');
+		            },
+		            select: function(evt,index){
+		            	//console.log(evt,index)
+		            }
+			        }).on('circleMenu-open',function(){
+		            //console.log('menu opened 2');
+			        });
+			        
+		
 						} else {
-							$('#btn_mega_gui').removeClass('push_button_pressed');
-							$('body').attr('id','');
+						
+							jQuery('#btn_mega_gui').removeClass('push_button_pressed');
+							
+							jQuery('#tools_top').attr('style',' '); // eliminamos los estilos del menu circular
+							jQuery('#tools_top').removeClass('circle_menu');
+							
+							setTimeout(function(){
+							jQuery('#tools_top .item').attr('style',' '); // eliminamos los estilos del menu circular
+							
+							jQuery('#tools_top > div').attr('style',' '); // eliminamos los estilos del menu circular
+							jQuery('#tools_top .push_button, #tool_top .tool_button').removeClass('item');
+							},250);
+							jQuery('body').attr('id','');
 						}
 					}
 				}
